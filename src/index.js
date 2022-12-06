3229bbd94b61c14b51bb4cfca8fedfd84402031d
Alexorcizet <alexorcizet87@gmail.com>
2022-12-06 09:49:51 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/main.scss';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import { store } from './store/store';
+import { RootCmp } from './root-cmp'
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <Router>
+      <RootCmp />
+    </Router>
+  </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

34ce6fe1cd9aef9a2676e7c29da5f758f87a2b19
Alexorcizet <alexorcizet87@gmail.com>
2023-01-24 10:10:01 +0200

    embed clip bug fixed

--- src/cmps/clip-dropdown.jsx
+++ src/cmps/clip-dropdown.jsx
@@ -7,18 +7,24 @@ export const ClipDropdown = ({ clip, station, onRemoveClip, setIsDropdownClip })
 
     const loggedInUser = useSelector(state => state.userModule.user)
     const [isShare, setIsShare] = useState(false)
-    const miniMediaPlayer = ''
+
+    const copyToClipBoard = (text) => {
+        console.log(clip);
+        navigator.clipboard.writeText(text)
+        console.log(text)
+    }
 
     return (
         <div className='dropdown-clip'>
             <ul>
                 {((station?.createdBy?._id === loggedInUser._id) || (loggedInUser?.isAdmin)) && <li className="remove-clip" onClick={(ev) => {
+                    ev.stopPropagation()
                     setIsDropdownClip(false)
                     onRemoveClip(ev, clip._id, clip.title)
                 }}>Remove from playlist</li>}
-                <li onClick={() => setIsShare(!isShare)}>Share</li>
+                {/* <li onClick={() => setIsShare(!isShare)}>Share</li> */}
                 {/* <li>Copy Song Link</li> */}
-                <li onClick={() => { navigator.clipboard.writeText(miniMediaPlayer) }}>Embed Track</li>
+                <li onClick={() => { copyToClipBoard('text') }}>Embed Track</li>
                 <li className="add-to-station">Add to playlist <DropDownList clip={clip} /></li>
             </ul>
         </div >
--- src/cmps/clip-preview.jsx
+++ src/cmps/clip-preview.jsx
@@ -31,6 +31,11 @@ export const ClipPreview = ({
         }
     }, [isPlaying])
 
+    const copyLink = (clipId) => {
+        navigator.clipboard.writeText(`https://www.youtube.com/watch?v=${clipId}`)
+
+    }
+
     const isCreatedAt = (type === 'search-res' || type === 'queue-clip')
     const dispatch = useDispatch()
 
@@ -75,36 +80,42 @@ export const ClipPreview = ({
         }}
 
         className={'clip-preview-container '} >
-        <div onClick={() => {
-            setIsClicked(!isClicked)
-            onTogglePlay(clip, isClicked, loggedInUser)
-        }} className='clip-preview-main-container'>
+        <div className='clip-preview-main-container'>
             {currClip?._id === clip?._id && isPlaying ? <div className='clip-equalizer'><img src={equalizer} onClick={() => {
                 setIsClicked(!isClicked)
+                onTogglePlay(clip, isClicked, loggedInUser)
             }} alt='clip-img' /></div> :
-                <div>
+                <div >
                     <i className={'clip-play-btn ' + (currClip?.id === clip?.id && isPlaying ? 'fas fa-pause' : 'fas fa-play playing')}></i>
                     <div className='clip-num'>{clipNum ? clipNum : idx + 1}</div>
                 </div>
             }
-            <div className='clip-title flex align-center'>
+            <div className='clip-title flex align-center' onClick={() => {
+                setIsClicked(!isClicked)
+                onTogglePlay(clip, isClicked, loggedInUser)
+            }}>
                 <img className='clip-img' src={clip.img?.url} alt='clip-img' />
                 <div className='title-text flex column'>
                     <h1 style={{ color: dndStyle?.color }}>{shortTitle(clip)}</h1>
                     <p style={{ color: dndStyle?.color }}>{clip.artist}</p>
                 </div>
             </div>
-            <div className='artist-name'>{clip.artist}</div>
+            <div className='artist-name' onClick={() => {
+                setIsClicked(!isClicked)
+                onTogglePlay(clip, isClicked, loggedInUser)
+            }}>{clip.artist}</div>
             {!isCreatedAt && <div className='added'>{clip.createdAt || clip.LikedAt || new Date(clip.addedAt * 1000).toLocaleDateString('he-IL')}</div>}
             {loggedInUser && <LikesBtns clip={clip} station={station} />}
             {clip.duration ? <div className='clock-area'>{getDuration(clip.duration)}</div> : ''}
             <i
                 className='dropdown-btn fa-solid fa-ellipsis'
-                onClick={() => setIsDropdownClip(!isDropdownClip)}>
-
+                onClick={() => {
+                    setIsDropdownClip(!isDropdownClip)
+                    copyLink(clip._id)
+                }}>
                 {isDropdownClip && <ClipDropdown
-                    station={station}
                     setIsDropdownClip={setIsDropdownClip}
+                    station={station}
                     onRemoveClip={onRemoveClip}
                     clip={clip}
                 />}
